516bd5eac0d873059edbf3f4609244eb00b29c37
antonio jurado <iberico.cortador@gmail.com>
2023-07-18 23:18:14 +0900

    Added Next.js functionality for interactive weather app

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Weather App</h1>
+      {/* Add your weather app components here */}
+    </div>
+  );
+}
